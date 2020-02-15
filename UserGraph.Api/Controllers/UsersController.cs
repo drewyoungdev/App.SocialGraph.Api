a658ef602b8703a42d47d0ac82d09232a064d3d2
Andrew Young <young.andrew2@outlook.com>
2020-02-15 13:31:15 -0500

    Initial gremlinq check-in

--- UserGraph.Api/Controllers/UsersController.cs
+++ UserGraph.Api/Controllers/UsersController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ExRam.Gremlinq.Core;
+using ExRam.Gremlinq.Core.GraphElements;
+using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Threading.Tasks;
 
 namespace UserGraph.Api.Controllers
@@ -6,10 +9,91 @@ namespace UserGraph.Api.Controllers
     [Route("api/[controller]")]
     public class UsersController : ControllerBase
     {
+        private IGremlinQuerySource _gremlinQuerySource;
+
+        public UsersController(IGremlinQuerySource gremlinQuerySource)
+        {
+            _gremlinQuerySource = gremlinQuerySource;
+        }
+
         [HttpGet]
         public async Task<IActionResult> Get()
         {
-            return Ok();
+            await _gremlinQuerySource.V().Drop();
+
+            var person1 = new Person()
+            {
+                Name = new VertexProperty<string, NameMeta>("Drew")
+                    {
+                        Properties = new NameMeta()
+                        {
+                            Creator = "User1",
+                            Date = DateTime.Now
+                        }
+                    },
+                Age = 125
+            };
+
+            var person2 = new Person()
+            {
+                Name = new VertexProperty<string, NameMeta>("Erica")
+                {
+                    Properties = new NameMeta()
+                    {
+                        Creator = "User2",
+                        Date = DateTime.Now
+                    }
+                },
+                Age = 126
+            };
+
+            var result = await _gremlinQuerySource
+                .AddV(person1)
+                .AddE<Knows>()
+                    .To(x => x.AddV(person2))
+                .FirstAsync();
+
+            return Ok(result);
         }
+
+        [HttpGet("{id}/knows")]
+        public async Task<IActionResult> Knows(string id)
+        {
+            Person[] knows = await _gremlinQuerySource
+                .V(id)
+                .Out<Knows>()
+                .OfType<Person>()
+                .ToArrayAsync();
+
+            return Ok(knows);
+        }
+    }
+
+    public class Person : Vertex
+    {
+        public int Age { get; set; }
+        public VertexProperty<string, NameMeta> Name { get; set; }
+    }
+
+    public class NameMeta
+    {
+        public string Creator { get; set; }
+        public DateTime Date { get; set; }
+    }
+
+    public class Vertex : IVertex
+    {
+        public object Id { get; set; }
+        public string Label { get; set; }
+        public string PartitionKey { get; set; } = "PartitionKey";
+    }
+
+    public class Knows : Edge
+    { }
+
+    public class Edge : IEdge
+    {
+        public object Id { get; set; }
+        public string Label { get; set; }
     }
 }
--- UserGraph.Api/Startup.cs
+++ UserGraph.Api/Startup.cs
@@ -1,11 +1,12 @@
+using ExRam.Gremlinq.Core;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System;
+using UserGraph.Api.Controllers;
 using UserGraph.Configuration;
-using UserGraph.DataLayer;
-using UserGraph.DataLayer.Interfaces;
 
 namespace UserGraph.Api
 {
@@ -23,15 +24,48 @@ namespace UserGraph.Api
         {
             services.Configure<AzureSettings>(Configuration.GetSection(nameof(AzureSettings)));
 
+            string gremlinUri = Configuration["AzureSettings:GremlinEndpoint"];
+            string gremlinDatabaseName = Configuration["AzureSettings:GremlinDatabaseName"];
+            string gremlinCollectionName = Configuration["AzureSettings:GremlinCollectionName"];
+            string gremlinAuthKey = Configuration["AzureSettings:GremlinAuthKey"];
+
+            services.AddSingleton<IGremlinQuerySource>(serviceProvider =>
+            {
+                return GremlinQuerySource.g
+                    //.AddStrategies(new PartitionKeyStrategy())
+                    .ConfigureEnvironment(env => env
+                        .UseModel(GraphModel.FromBaseTypes<Vertex, Edge>(lookup => lookup.IncludeAssembliesOfBaseTypes()))
+                        .ConfigureModel(model => model
+                            //.ConfigureElements(elem => elem.UseCamelCaseLabels())
+                            .ConfigureProperties(prop => prop.UseCamelCaseNames()))
+                        )
+                        //.UseExecutionPipeline(GremlinQueryExecutionPipeline.EchoGroovy)
+                        //.UseLogger(logger)
+                    .UseCosmosDb(
+                        new Uri(gremlinUri), 
+                        gremlinDatabaseName, 
+                        gremlinCollectionName, 
+                        gremlinAuthKey
+                     );
+            });
+
             services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            var builder = new ConfigurationBuilder()
+               .SetBasePath(env.ContentRootPath)
+               .AddJsonFile("appsettings.json",
+                            optional: false,
+                            reloadOnChange: true)
+               .AddEnvironmentVariables();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                builder.AddUserSecrets<Startup>();
             }
 
             //app.UseHttpsRedirection();
@@ -46,4 +80,13 @@ namespace UserGraph.Api
             });
         }
     }
+
+    // https://medium.com/@jayanta.mondal/getting-the-best-out-of-cosmos-dbs-scale-out-graph-api-aka-partitioned-graph-containers-bf47c240e698
+    //public class PartitionKeyStrategy : IGremlinQueryStrategy
+    //{
+    //    public IGremlinQueryBase Apply(IGremlinQueryBase query)
+    //    {
+    //        throw new NotImplementedException();
+    //    }
+    //}
 }
